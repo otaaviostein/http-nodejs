972d463ac17ee98e7a4ff9841ad9bbd9a8c45550
otavio.stein <otavio.stein@fulltechpro.com.br>
2023-03-28 23:04:43 -0300

    ajustes

--- server.mjs
+++ server.mjs
@@ -1,6 +1,20 @@
-import { createServer } from 'http';
+// import { createServer } from 'http';
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+// createServer((req, res) => {
+//   res.write('Hello World!');
+//   res.end();
+// }).listen(process.env.PORT);
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+
+app.get("/", (req,res)=> {
+    res.send("Hello there! Api is working")
+})
+
+app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
